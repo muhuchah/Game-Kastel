0b73980bd3e0397978d776cd8d1c0a53c934f3fb
lash <m.h.chah4@gmail.com>
2022-03-04 17:47:31 +0330

    gameSetting function added

--- main.cpp
+++ main.cpp
@@ -6,8 +6,51 @@
 
 using namespace std;
 
+void start();
+void gameSetting();
+
 int main()
 {
-    cout << "++++++++ Game Kastel ++++++++";
+    cout << "++++++++ Game Kastel ++++++++\n" << endl
+         << "1-- Start" << endl
+         << "2-- Help " << endl
+         << "0-- Exit " << endl
+         << "Please Enter Your Choice --> ";
+    int enter;
+    cin >> enter;
+    if( enter == 0 )
+        exit(0);
+    if( enter == 1 )
+        start();
+    else
+        help();
+
     return 0;
+}
+
+void start()
+{
+    gameSetting();
+}
+
+void help()
+{}
+
+void gameSetting()
+{
+    system("cls");
+    cout << "++++++++ Game Setting ++++++++\n" << endl
+        << "Number of Players : ";
+   int numberOfPlayers;
+   cin >> numberOfPlayers;
+   cout << "\nFinish Score : ";
+   int victoryScore;
+   cin >> victoryScore;
+   cout << "\n* Which Mode *" << endl
+        << "1-- Standard Mode \n"
+        << "2-- Pro Mode \n"
+        << "--> ";
+   int gameMode;
+   cin >> gameMode;
+
 }
