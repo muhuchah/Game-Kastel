7d664f1d0dfb9a27454b389c8a7341d929100b3e
lash <m.h.chah4@gmail.com>
2022-03-04 14:47:11 +0330

    Create Game File

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// In The Name Of God
+// Game Kastel
+// Muhammad Hussein Chahkandi
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "++++++++ Game Kastel ++++++++";
+    return 0;
+}
