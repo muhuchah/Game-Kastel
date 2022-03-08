99bb10597f3b4b22b648411ccfc120c48300d104
lash <m.h.chah4@gmail.com>
2022-03-08 08:17:39 +0330

    dealCard function added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 // Muhammad Hussein Chahkandi
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -10,13 +12,13 @@ void start();
 void help();
 void gameSetting();
 void runGame( int, int, int );
+void dealCard( int (&)[90], int (&)[90] );
 void printYavarCard1Digit( int );
 void printYavarCard2Digits( int );
 void printSafiCard1Digit( int );
 void printSafiCard2Digits( int );
 void printMofradCard1Digit( int );
 void printMofradCard2Digits( int );
-
 
 int main()
 {
@@ -73,21 +75,14 @@ void gameSetting()
         << "--> ";
    int gameMode;
    cin >> gameMode;
-   runGame( numberOfPlayers, victoryScore, gameMode );
+
+    runGame( numberOfPlayers, victoryScore, gameMode );
 }
 
 void runGame( int numberOfPlayers, int victoryScore, int gameMode )
 {
-    for( int i = 1; i <= numberOfPlayers; i++ )
-    {
-        system("cls");
-        cout << "***| Player Number "
-             << i << " |***" << endl;
-        for( int cardNumber = 0; cardNumber < 3; cardNumber++ )
-        {
-
-        }
-    }
+    int cardsNumber[90], cardsChar[90];
+    dealCard( cardsNumber, cardsChar );
 }
 void printYavarCard1Digit( int n )
 {
@@ -166,4 +161,73 @@ void printMofradCard2Digits( int n )
          << "|               |" << endl
          << "|            "<< n << " |" << endl
          << "|_ _ _ _ _ _ _ _|" << endl;
+}
+
+void dealCard( int (&cardsNumber)[90], int (&cardsChar)[90] )
+{
+    int cardCounter{}, numberTemp, charTemp;
+    for( int i = 0; i < 90; i++ )
+    {
+        bool cardsNotFree;
+        srand(time(0));
+        do
+        {
+            cardsNotFree = false;
+            numberTemp = rand() % 15 + 1;
+            charTemp = rand() % 3 + 1;
+            for( int j = 0; j < cardCounter; j++ )
+            {
+                if( numberTemp == cardsNumber[j] )
+                {
+                    if( charTemp == cardsChar[j] )
+                    {
+                        for( int r = j+1; r < cardCounter; r++ )
+                        {
+                            if( numberTemp == cardsNumber[r] )
+                            {
+                                if( charTemp == cardsChar[r] )
+                                {
+                                    cardsNotFree = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }while( cardsNotFree );
+        cardsNumber[cardCounter] = numberTemp;
+        cardsChar[cardCounter] = charTemp;
+        cardCounter++;
+    }
+
+    int temp[90], temp1[90], temp2[90];
+    bool te;
+    for( int i = 0; i < 90; i++ )
+    {
+        te = false;
+        int t;
+        t = rand() % 89;
+        for( int j = 0; j < i; j++ )
+        {
+            if( t == temp2[j] )
+            {
+                i--;
+                te = true;
+                break;
+            }
+            else
+            {
+                temp2[i] = t;
+            }
+        }
+        if( te )
+            continue;
+        temp[i] = cardsNumber[t];
+        temp1[i] = cardsChar[t];
+    }
+    for( int i = 0; i < 90; i++ )
+    {
+        cardsNumber[i] = temp[i];
+                cardsChar[i] = temp1[i];
+    }
 }
